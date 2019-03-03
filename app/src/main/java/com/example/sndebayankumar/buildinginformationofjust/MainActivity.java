f4b3c09c6c79097680deb7fa084d87f754d819ea
SN DEBAYAN KUMAR <debayanmondal.just@gmail.com>
2019-03-04 00:46:42 +0600

    first commit

--- app/src/main/java/com/example/sndebayankumar/buildinginformationofjust/MainActivity.java
+++ app/src/main/java/com/example/sndebayankumar/buildinginformationofjust/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.sndebayankumar.buildinginformationofjust;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       MenuInflater menuInflater= getMenuInflater();
+       menuInflater.inflate(R.menu.main_menu, menu);
+       return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.AdminId) {
+            Toast.makeText(MainActivity.this, "Admin is selected",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if(item.getItemId()==R.id.LogoutId) {
+            Toast.makeText(MainActivity.this, "Logout is selected",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
